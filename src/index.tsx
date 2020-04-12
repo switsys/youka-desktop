cdf47a097563fba3a7baf877cccd1892265f0fd3
youka <youka.club@gmail.com>
2020-04-12 08:49:47 +0300

    fullstory

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as FullStory from "@fullstory/browser";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+if (process.env.NODE_ENV === "production") {
+  FullStory.init({ orgId: "TCK6J" });
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.register();
